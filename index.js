60a6979086471d4050e833a524be4312290c9151
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-09-21 11:37:32 +0800

    findplayer

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = new Server({
   cors: "*",
 });
 const allUser = {};
-
+let allPlayer = [];
 io.on("connection", (socket) => {
   allUser[socket.id] = {
     socket: socket,
@@ -16,56 +16,94 @@ io.on("connection", (socket) => {
   };
 
   socket.on("join", (data) => {
+    allPlayer.push({
+      socketId: socket.id,
+      playerName: data.playerName,
+      playing: false,
+      online: true,
+    });
     const currentUser = allUser[socket.id];
     currentUser.playerName = data.playerName;
 
+    io.emit("allUser", allPlayer);
+
     let recipientPlayer;
-    for (const key in allUser) {
-      const user = allUser[key];
-      if (user.online && !user.playing && socket.id !== key) {
-        recipientPlayer = user;
-        user.playing = true;
-        currentUser.playing = true;
-        break;
+    socket.on("findPlayer", (socketId) => {
+      console.log(socketId);
+      recipientPlayer = allUser[socketId];
+      if (recipientPlayer) {
+        allPlayer.map((item) => {
+          if (item.socketId === recipientPlayer.socket.id) {
+            item.playing = true;
+          } else if (item.socketId === currentUser.socket.id) {
+            item.playing = true;
+          }
+        });
+
+        io.emit("allUser", allPlayer);
+        recipientPlayer.socket.emit("recipientPlayerFound", {
+          recipientName: currentUser.playerName,
+          playingAs: "X",
+        });
+        currentUser.socket.emit("recipientPlayerFound", {
+          recipientName: recipientPlayer.playerName,
+          playingAs: "O",
+        });
+
+        currentUser.socket.on("playerMove", (data) => {
+          recipientPlayer.socket.emit("movePlayerFromServer", data);
+        });
+
+        recipientPlayer.socket.on("playerMove", (data) => {
+          currentUser.socket.emit("movePlayerFromServer", data);
+        });
+        currentUser.socket.on("finish", (data) => {
+          recipientPlayer.playing = false;
+          recipientPlayer.online = false;
+          allPlayer.map((item) => {
+            if (item.socketId === recipientPlayer.socket.id) {
+              item.playing = false;
+            } else if (item.socketId === currentUser.socket.id) {
+              item.playing = false;
+            }
+          });
+          setTimeout(() => {
+            io.emit("allUser", allPlayer);
+          }, 3000);
+
+          recipientPlayer.socket.emit("finishState", data);
+        });
+
+        recipientPlayer.socket.on("finish", (data) => {
+          currentUser.playing = false;
+          currentUser.online = false;
+          allPlayer.map((item) => {
+            if (item.socketId === recipientPlayer.socket.id) {
+              item.playing = false;
+            } else if (item.socketId === currentUser.socket.id) {
+              item.playing = false;
+            }
+          });
+          setTimeout(() => {
+            io.emit("allUser", allPlayer);
+          }, 3000);
+          currentUser.socket.emit("finishState", data);
+        });
+      } else {
+        currentUser.socket.emit("recipientPlayerNotFound");
       }
-    }
-
-    if (recipientPlayer) {
-      recipientPlayer.socket.emit("recipientPlayerFound", {
-        recipientName: currentUser.playerName,
-        playingAs: "X",
-      });
-      currentUser.socket.emit("recipientPlayerFound", {
-        recipientName: recipientPlayer.playerName,
-        playingAs: "O",
-      });
-
-      currentUser.socket.on("playerMove", (data) => {
-        recipientPlayer.socket.emit("movePlayerFromServer", data);
-      });
-
-      recipientPlayer.socket.on("playerMove", (data) => {
-        currentUser.socket.emit("movePlayerFromServer", data);
-      });
-      currentUser.socket.on("finish", (data) => {
-        recipientPlayer.playing = false;
-        recipientPlayer.online = false;
-        recipientPlayer.socket.emit("finishState", data);
-      });
-
-      recipientPlayer.socket.on("finish", (data) => {
-        currentUser.playing = false;
-        currentUser.online = false;
-        currentUser.socket.emit("finishState", data);
-      });
-    } else {
-      currentUser.socket.emit("recipientPlayerNotFound");
-    }
+    });
   });
 
   socket.on("disconnect", () => {
     const currentUser = allUser[socket.id];
     currentUser.online = false;
+    allPlayer.filter((item) => {
+      if (item.socketId === socket.id) {
+        item.online = false;
+      }
+    });
+    io.emit("allUser", allPlayer);
   });
 });
 let port = process.env.PORT || 5000;
