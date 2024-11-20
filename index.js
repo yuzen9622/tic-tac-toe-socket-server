77ca96f9705976bdcd5b768c37b0f8f763dab40a
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-11-20 18:04:19 +0800

    bug

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const { Server } = require("socket.io");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const http = require("http");
-const dotenv = require("dotenv");
 
 /*socket server*/
 const io = new Server({
@@ -117,62 +114,3 @@ io.on("connection", (socket) => {
 let port = process.env.PORT || 5000;
 console.log("socket server online at ", port);
 io.listen(port);
-/*web server*/
-
-dotenv.config();
-const server = http.createServer(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  if (req.url === "/play" && req.method === "POST") {
-    let body = "";
-
-    req.on("data", (chunk) => {
-      body += chunk.toString(); // chunk 是 Buffer，需要轉成字串
-    });
-    req.on("end", async () => {
-      console.log(JSON.parse(body).board);
-      body = JSON.parse(body);
-      const board = body.board;
-      const genAI = new GoogleGenerativeAI(process.env.AISTUDIO_API_KEY);
-      const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-002" });
-      const generationConfig = {
-        temperature: 0,
-        topP: 0.95,
-        topK: 40,
-        maxOutputTokens: 8192,
-        responseMimeType: "application/json",
-      };
-
-      const chatSession = model.startChat({
-        generationConfig,
-        history: [
-          {
-            role: "user",
-            parts: [
-              {
-                text: "play tictactoe with me and board ['O','X','','','','','','O',''] and you are player X and O first and response me 0~8 position",
-              },
-            ],
-          },
-          {
-            role: "model",
-            parts: [
-              {
-                text: '```json\n{"board": ["O", "X", "X", "", "", "", "", "O", ""], "position": 2}\n\n```',
-              },
-            ],
-          },
-        ],
-      });
-
-      const result = await chatSession.sendMessage(` ${board}`);
-      const response = JSON.parse(result.response.text());
-      console.log(response);
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(result.response.text());
-    });
-  } else {
-    res.end("404 Not found");
-  }
-});
-server.listen(8000, () => console.log("server is run at port 8000"));
